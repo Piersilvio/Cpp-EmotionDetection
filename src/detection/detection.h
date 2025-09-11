c6085cb0dd26d78ea2ed9ea2504fb3d2de96f51a
tommyballa <ballarintom@gmail.com>
2025-09-11 14:08:54 +0200

    Update detection.h

--- src/detection/detection.h
+++ src/detection/detection.h
@@ -9,7 +9,7 @@ extern const std::string FACE_DETECTOR_MODEL_PATH;
 
 
 // Detect faces
-extern void detect_face(cv::Mat& input_image);
+extern void detect_face(cv::Mat& input_image,const std::vector<cv::Rect>& ground_truth_faces);
     
 // Draw a box sorrounding the detected face
 Image draw_face_box(cv::Mat& input_image);
