63603eee448d7cd8be43ed9dd0c8b13f27bbaa81
jackbonaventura <56650936+jackbonaventura@users.noreply.github.com>
2025-09-16 01:38:29 +0200

    Add files via upload

--- src/emotion_recognition/emotion_recognition.h
+++ src/emotion_recognition/emotion_recognition.h
@@ -1,33 +1,24 @@
+/*
+ *  Pre-processing of images containing ROIs, mapping of class ID to the corresponding emotion
+ *  label, loading of the model, performing inference.
+ */
+ 
 #ifndef EMOTION_H
 #define EMOTION_H
-
-
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 #include "../image/Image.h"
 
-/**
- * Model class that contains code to read the pretrained tensorflow model and allows
- * us to make predictions on new images
- */
+// Preprocess ROIs before giving them in input to the model for emotion recognition
+void preprocessROI(std::vector<cv::Mat>& _roi_image, Image& img);
 
-    // Constructor reads in a pretrained (in python) tensorflow graph and weights (.pb file contains everything we need about the model)
-    // Also initialises the mapping from class id to the string label (ie. happy, angry, sad etc)  
-
-
-
-// Model inference function takes image input and outputs the prediction label and the probability
-std::vector<std::string> predict(Image& image, std::string model_filename);
-
-
-// Function to preprocess image for model input
-void preprocessROI(std::vector<cv::Mat>& _roi_image, Image& image_and_ROI);
+// Load model and run model inference
+std::vector<std::string> predict(Image& img, std::string model);
 
 // Print the predicted emotion label
 Image print_predicted_label(Image& image_and_ROI, std::vector<std::string>& emotion_prediction, std::vector<cv::Rect> detected_faces);
-
 
 #endif
 
--- src/emotion_recognition/pre-processing.cpp
+++ src/emotion_recognition/pre-processing.cpp
@@ -1,3 +1,7 @@
+/*
+ *  Pre-processing of the images containing ROIs
+ */
+ 
 #include <opencv2/opencv.hpp>
 
 #include "../image/Image.h"
@@ -5,41 +9,40 @@
 
 using namespace cv;
 
-void preprocessROI(std::vector<Mat>& roi_image, Image& image_and_ROI) {
+void preprocessROI(std::vector<Mat>& ROI_images, Image& img) {
 
     Mat processed_image;
     std::vector<Mat>  preprocessed_ROI;
 
-    if (roi_image.empty()) {
+    if (ROI_images.empty()) {
         std::cerr << "No ROI found, skip preprocessing." << std::endl;
-        image_and_ROI.set_preprocessed_ROI(preprocessed_ROI);
+        img.set_preprocessed_ROI(preprocessed_ROI);
         return;
     }
 
-    if (roi_image.size() > 0) { 
-        for (int i=0; i < roi_image.size(); i++) {
+    if (ROI_images.size() > 0) { 
+        for (int i=0; i < ROI_images.size(); i++) {
         
-                if (roi_image[i].empty()) {
-            std::cerr << "ROI " << i << " empty, skipped." << std::endl;
-            continue;
-        }
+            if (ROI_images[i].empty()) {
+                std::cerr << "ROI " << i << " empty, skipped." << std::endl;
+                continue;
+            }
 
-                // convert to grayscale 
-                Mat gray_image;
-                cvtColor( roi_image[i], gray_image, COLOR_BGR2GRAY );
+            // Convert to grayscale 
+            Mat gray_image;
+            cvtColor(ROI_images[i], gray_image, COLOR_BGR2GRAY );
 
-                // Resize the ROI to model input size
-                resize(gray_image, processed_image, Size(48,48));
+            // Resize the ROI to model input size
+            resize(gray_image, processed_image, Size(48,48));
 
-                // Convert image pixels from between 0-255 to 0-1
-                processed_image.convertTo(processed_image, CV_32FC3, 1.f/255);
-
-                // Append onto the model input vector
-                preprocessed_ROI.push_back(processed_image);
+            // Convert image pixels from between 0-255 to 0-1
+            processed_image.convertTo(processed_image, CV_32FC3, 1.f/255);
+            
+            preprocessed_ROI.push_back(processed_image);
         }
     }
     
-    image_and_ROI.set_preprocessed_ROI(preprocessed_ROI); 
+    img.set_preprocessed_ROI(preprocessed_ROI); 
     
     return;
 
